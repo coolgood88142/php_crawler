afd789de0be781d4a5680c9af426179a42cdcd3f
Kai <coolgood88142@gmail.com>
2019-06-23 17:03:50 +0800

    06/23

--- crawler_composer.php
+++ crawler_composer.php
@@ -4,18 +4,29 @@ require	'vendor/autoload.php';
 use Goutte\Client;
 use GuzzleHttp\Client as GuzzleClient;
 
-$client = new Client();
+try{
+	$title[0] = array();
+	$title[1] = array();
 
-$crawler = $client->request('GET', 'https://tw.yahoo.com/');
-$i=1;$j=1;
+	$client = new Client();
+	$client = new GuzzleClient(array(
+	    'timeout' => 60,
+	));
 
-$link = $crawler->filter('a > span[class="Va-tt"]')->each(function ($node) {
-    return $node->text();
-});
+	$crawler = $client->request('GET', 'https://tw.yahoo.com/');
+	$i=1;$j=1;
 
-$subtitle = $crawler->filter('p > span[class="Va-tt"]')->each(function ($node) {
-    return $node->text();
-});
+	$link = $crawler->filter('<span class="D-b Fl-end Pos-r Ov-h"]')->each(function ($node) {
+	    return $node->text();
+	});
 
-$title = array($link,$subtitle);
+	$subtitle = $crawler->filter('p > span[class="Va-tt"]')->each(function ($node) {
+	    return $node->text();
+	});
+
+	$title = array($link,$subtitle);
+
+}catch(Exception $e){
+	echo "讀取時間過長";
+}
 ?>
