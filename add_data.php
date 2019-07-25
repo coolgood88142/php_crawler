7188f83430da257d116e8f2f2f2c296e36666766
Kai <coolgood88142@gmail.com>
2019-07-26 03:14:19 +0800

    PHP爬蟲yahoo標題資料更新時要新增新的資料(未完成: 迴圈比對資料還沒寫好)

--- add_data.php
+++ add_data.php
@@ -1,22 +1,49 @@
 <?php
     include("mysql.php");
-    $sql = "INSERT INTO title (ti_category, ti_name, ti_text)
-            VALUES ";
-            
-    $data = "";
-    for($i=0;$i<$category_count;$i++){
-        $ti_category =  $category_title[$i];
-        $a = $i * 4;
-        $b = $a + 4;
+    date_default_timezone_set('Asia/Taipei');
+    $ti_date = date ("Y-m-d");
+    $data = "";$name = "";
 
-        for($j=$a;$j<$b;$j++){
-            $ti_name = $yahoo_title[$j];
-            $ti_text = $yahoo_subtitle[$j];
+    $sql = "SELECT count(ti_id) as ti_count FROM title";
+    $query = $conn->query($sql);
+    $count = $query->fetchAll(PDO::FETCH_ASSOC);
+    $count = $category_count-$count;
 
-            $data = $data . '(' . '\''.$ti_category.'\'' . ',' . '\''.$ti_name.'\''. ',' . '\''.$ti_text.'\''. '),';
+    if($count>0){
+        for($i=0;$i<$category_count;$i++){
+            $ti_category =  $category_title[$i];
+            $a = $i * 4;
+            $b = $a + 4;
+
+            for($j=$a;$j<$b;$j++){
+                $ti_name = $yahoo_title[$j];
+                $ti_text = $yahoo_subtitle[$j];
+
+                // $data = $data . '(' . '\''.$ti_category.'\'' . ',' . '\''.$ti_name.'\''. ',' . '\''.$ti_text.'\''. '),';
+                $data = $data . "('$ti_category', '$ti_date', '$ti_name', '$ti_text'),";
+                $name = $name . '\'' . $ti_name . '\',';
+            }
         }
     }
 
+    
+
+    $name = substr($name, 0, -1);
+    $sql = "SELECT ti_id FROM title WHERE ti_name NOT IN ($name)";
+    $query = $conn->query($sql);
+    $ti_id = $query->fetch(PDO::FETCH_ASSOC);
+    $count=count($ti_id);
+
+    if($count>0){
+        for($i=0 ; $i<$count ; $i++){
+            $id = $ti_id[$i];
+        }
+    }
+    
+    
+    $sql = "INSERT INTO title (ti_category, ti_date, ti_name, ti_text)
+            VALUES ";
+
     $sql = $sql . substr($data, 0, -1);
-    $conn->exec($sql);
+    // $conn->exec($sql);
 ?>
